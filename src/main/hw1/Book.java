1780500d2f55ceb44780cb89c66c1ec840f52c02
JaushevEhM <eduard.yaushev@akbarsdigital.ru>
2021-06-13 17:31:45 +0300

    Добавлены свойства и методы

--- src/main/hw1/Book.java
+++ src/main/hw1/Book.java
@@ -1,5 +1,27 @@
 package main.hw1;
 
 public class Book {
+    public String title;
+    public int pageCount;
+    public double price;
+    public boolean isOpened;
 
+    public void open(){
+        if (isOpened)
+            System.out.println("Книга уже открыта");
+        else {
+            isOpened = true;
+            System.out.println("Вы открыли книгу");
+        }
+
+    }
+
+    public void close(){
+        if (!isOpened)
+            System.out.println("Книга уже закрыта");
+        else {
+            isOpened = false;
+            System.out.println("Вы закрыли книгу");
+        }
+    }
 }
