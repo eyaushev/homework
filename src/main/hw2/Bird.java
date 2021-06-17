6141f47a1133e6130efaa419efd329a72f6fab85
JaushevEhM <eduard.yaushev@akbarsdigital.ru>
2021-06-17 19:27:52 +0300

    HW2: Создан абстрактный класс Bird с приватными св-вами

---
+++ src/main/hw2/Bird.java
@@ -0,0 +1,31 @@
+package main.hw2;
+
+public abstract class Bird {
+    private String name;
+    private String family;
+    private String order;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+}
