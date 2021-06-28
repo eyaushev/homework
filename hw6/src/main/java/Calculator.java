852fa643d9bf99fa3673efdf8a30e77e2de32ae4
JaushevEhM <eduard.yaushev@akbarsdigital.ru>
2021-06-28 21:26:11 +0300

    Создан класс Calculator c методами

---
+++ hw6/src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public double sum(double x, double y){
+        return x + y;
+    }
+
+    public double minus(double x, double y){
+        return x - y;
+    }
+
+    public double multiple(double x, double y){
+        return x * y;
+    }
+
+    public double divise(double x, double y){
+        return x/y;
+    }
+}
